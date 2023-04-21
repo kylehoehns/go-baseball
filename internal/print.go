f4313e9efc425f1579acb0f161f1bcf5340d1c90
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2023-04-21 22:53:59 +0000

    update printout to be proper formatted markdown table

--- internal/print.go
+++ internal/print.go
@@ -6,40 +6,34 @@ import (
 )
 
 func PrintRoster(roster *Roster) {
-	fmt.Println("Player Names:")
-	printPlayerNames(roster)
-	fmt.Println("\nBatting Order:")
+	fmt.Println("## Batting Order:")
 	printBattingOrderTable(roster)
-	fmt.Println("\nFielding Positions:")
+	fmt.Println("\n## Fielding Positions:")
 	printFieldingPositionsTable(roster)
-}
-
-func printPlayerNames(roster *Roster) {
-	for i, name := range roster.PlayerNames {
-		fmt.Printf("%d. %s\n", i+1, name)
-	}
 }
 
 func printBattingOrderTable(roster *Roster) {
 	innings := len(roster.BattingOrder)
-	header := "Inning | " + strings.Join(makeRange(1, innings+1, "%-10d"), " | ")
+	header := "| Inning | " + strings.Join(makeRange(1, innings+1, "%-10d"), " | ")
 	fmt.Println(header)
-	fmt.Println(strings.Repeat("-", len(header)))
+	separator := "|--------|" + strings.Repeat("------------|", innings)
+	fmt.Println(separator)
 
 	for i := 0; i < len(roster.PlayerNames); i++ {
 		row := make([]string, innings)
 		for inning, order := range roster.BattingOrder {
 			row[inning-1] = fmt.Sprintf("%-10s", order[i])
 		}
-		fmt.Println("       | " + strings.Join(row, " | "))
+		fmt.Println("|        | " + strings.Join(row, " | "))
 	}
 }
 
 func printFieldingPositionsTable(roster *Roster) {
 	innings := len(roster.FieldingPositions)
-	header := "Inning | " + strings.Join(makeRange(1, innings+1, "%-10d"), " | ")
+	header := "| Inning | " + strings.Join(makeRange(1, innings+1, "%-10d"), " | ")
 	fmt.Println(header)
-	fmt.Println(strings.Repeat("-", len(header)))
+	separator := "|--------|" + strings.Repeat("------------|", innings)
+	fmt.Println(separator)
 
 	positions := []string{"P", "1B", "2B", "3B", "SS", "LF", "CF", "RF"}
 	for _, position := range positions {
@@ -47,7 +41,7 @@ func printFieldingPositionsTable(roster *Roster) {
 		for inning, posMap := range roster.FieldingPositions {
 			row[inning-1] = fmt.Sprintf("%-10s", posMap[position])
 		}
-		fmt.Printf("%-6s | %s\n", position, strings.Join(row, " | "))
+		fmt.Printf("| %-6s | %s\n", position, strings.Join(row, " | "))
 	}
 }
 
