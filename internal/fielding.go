75b045ebb73848fefebaf51d48ec9f390bad79e7
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2024-04-26 16:25:41 -0500

    simplify fielding positions logic

--- internal/fielding.go
+++ internal/fielding.go
@@ -5,49 +5,25 @@ import (
 )
 
 func AddFieldingPositions(roster *Roster, innings int) *Roster {
-	group1 := []string{"LF", "RF"}
-	if len(roster.PlayerNames) == 8 {
-		group1 = append(group1, "CF")
-	}
-	group2 := []string{"P", "1B", "2B", "SS", "3B"}
-	fieldingPositions := make(map[int]map[string]string, innings)
 
-	initialPositions := make(map[string]string)
-	for i, name := range roster.PlayerNames {
-		initialPositions[name] = group1[i%len(group1)]
-	}
+	fieldingPositions := []string{"LF", "RF", "CF", "P", "1B", "2B", "SS", "3B"}
 
-	firstGroupRotationAmount := 1
-	secondGroupRotationAmount := 4
-	if len(roster.PlayerNames) != 8 {
-		firstGroupRotationAmount = 2
-		secondGroupRotationAmount = 5
-	}
+	finalPositions := make(map[int]map[string]string, innings)
 
 	for inning := 1; inning <= innings; inning++ {
 		inningPositions := make(map[string]string)
 
 		// Rotate positions within each group
-		rotatedGroup1 := utils.Rotate(group1, firstGroupRotationAmount*(inning-1))
-		rotatedGroup2 := utils.Rotate(group2, secondGroupRotationAmount*(inning-1))
-
-		// Switch between groups each inning
-		if inning%2 == 0 {
-			rotatedGroup1, rotatedGroup2 = rotatedGroup2, rotatedGroup1
-		}
+		rotatedGroup := utils.Rotate(fieldingPositions, 3*(inning-1))
 
 		// Assign positions to players
 		for i, name := range roster.PlayerNames {
-			if i < len(rotatedGroup1) {
-				inningPositions[rotatedGroup1[i]] = name
-			} else {
-				inningPositions[rotatedGroup2[i-len(rotatedGroup1)]] = name
-			}
+			inningPositions[rotatedGroup[i]] = name
 		}
 
-		fieldingPositions[inning] = inningPositions
+		finalPositions[inning] = inningPositions
 	}
 
-	roster.FieldingPositions = fieldingPositions
+	roster.FieldingPositions = finalPositions
 	return roster
 }
